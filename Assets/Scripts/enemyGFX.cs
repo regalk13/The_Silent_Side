b040c7d5c1de0972ce2ecebc8f820da80115797f
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-04-06 20:47:58 -0500

    new things

---
+++ Assets/Scripts/enemyGFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+     float distance = Mathf.Abs(player.transform.position.x - transform.position.x);
+     if(aiPath.desiredVelocity.x >= 0.01f)
+     {
+         transform.localScale = new Vector3(-1, 1f, 1f);
+     }else if (aiPath.desiredVelocity.x <= -0.01f)
+     {
+         transform.localScale = new Vector3(1f, 1f, 1f);
+     }
+    }
+}
--- Assets/Scripts/girlmove.cs
+++ Assets/Scripts/girlmove.cs
@@ -11,22 +11,27 @@ public class girlmove : MonoBehaviour
     private Animator Animator;
     private float Horizontal;
     private bool Grounded;
-
+    private AudioSource footstep;
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
+        footstep = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-          
+        if (Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (Horizontal > 0.0f) 
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);     
+        }
         Animator.SetBool("running", Horizontal != 0.0f);
     
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
@@ -70,5 +75,9 @@ public class girlmove : MonoBehaviour
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+
+    private void Footstep(){
+        footstep.Play();
     }
 }
