7b1991eca1d16e0211254651ba665999cce729d7
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-04-13 17:06:10 -0500

    new

---
+++ Assets/Scripts/loadscene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscene : MonoBehaviour
+{
+    public int Respawn;
+    
+    public void scene()
+    {
+        SceneManager.LoadScene(Respawn);
+    }
+}
