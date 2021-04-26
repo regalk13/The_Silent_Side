11ba773aff16c0ab43ad35681269002550de09aa
Juan Salazar <jsalazarcaviedes30@gmail.com>
2021-04-26 16:28:33 -0500

    new dialogs

---
+++ Assets/Scripts/loadcredits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadcredits : MonoBehaviour
+{
+    public int Respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (showTextFuntion());
+    }
+
+    IEnumerator showTextFuntion(){
+        yield return new WaitForSeconds (59f);
+        SceneManager.LoadScene(Respawn);
+        
+    }
+}
